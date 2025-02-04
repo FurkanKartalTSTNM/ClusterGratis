5f2490fecea0d74502c7ddf094a019715dab2bd7
Furkan Kartal <Furkan.kartal@testinium.com>
2025-02-04 16:15:26 +0300

    deneme

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -133,6 +133,7 @@ public class HookImpl {
                 capabilities.setCapability("key", System.getenv("key"));
                 capabilities.setCapability("waitForAppScript", "$.delay(1000);");
                 capabilities.setCapability("bundleId", "com.pharos.Gratis");
+                capabilities.setCapability("waitForQuiescence", false);
                 //capabilities.setCapability("bundleId", "com.pharos.gratis.uat");
                 capabilities.setCapability("usePrebuiltWDA",true);
                 capabilities.setCapability("useNewWDA", false);
